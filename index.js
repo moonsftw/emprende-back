a36eebd5c99ae290f908c15fe246e3ecb5d366f0
Facu Luna <nfacundoluna@gmail.com>
2023-11-14 21:31:13 -0300

    no había importado el dotenv

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //const importadoDeAnother = require("./another");
-
+require('dotenv').config()
 const http = require("http");
 
 
@@ -13,4 +13,6 @@ const server = http.createServer(requestController);
 
 //server.listen(4000);
 const PORT = process.env.PORT
-server.listen(PORT)
+server.listen(PORT, function () {
+    console.log("Aplicación corriendo en el puerto: " + PORT);
+})
